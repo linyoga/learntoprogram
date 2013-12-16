c68bc4bf047e4da50573964a4a04d98c3f7b5d28
linyoga <linyoga@gmail.com>
2013-12-16 17:53:40 +0800

    chapter 2

---
+++ chapter2.rb
@@ -0,0 +1,4 @@
+puts "Hours in a year",(365 * 24)
+puts "Minutes in a decade" ,((365 * 10 + 2) * 24  * 60)
+puts "Your age in seconds" , (365 * 24 * 60 * 60 * 27)
+puts "Our dear author's age" , (1025000000 / (365 * 24 * 60 * 60))
